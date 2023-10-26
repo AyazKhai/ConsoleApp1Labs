acfa5a3c996c1d97b9c442f6ad963dd4608ae77b
AyazKhai <ayazhhh04@gmail.com>
2023-10-27 00:40:15 +0300

    рр

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -15,11 +15,11 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Введите кол-во элементов");
             int n = Convert.ToInt32(Console.ReadLine());
-            queues a = new queues();
+            queues a = new queues(n);
+            
 
             for(int i = 0; i < n; i++) 
             {
-                a[i] = new PowerSuply();
                 Console.WriteLine($"Источник автономного питания:\nФирма: {a[i].firm}\nМарка: {a[i].mark}\nИдентифиционный номер: {a[i].id}\n");
             }
             Console.ReadKey();
@@ -30,12 +30,25 @@ namespace ConsoleApp1
 class queues
 {
     PowerSuply[] Queues = new PowerSuply[30];
+    public queues(int n) 
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Queues[i] = new PowerSuply();
+        }
+    }
+
     public PowerSuply this[int pos]
     {
         get
         {
-            if (pos >= 0 && pos < 30)
+            if (pos >= 0 && pos < 30) 
+            {
+                
                 return Queues[pos];
+
+            }
+                
             else
             {
                 throw new IndexOutOfRangeException("Вне диапазона");
@@ -51,7 +64,7 @@ public class PowerSuply
 {
     public string firm;
     public string mark;
-    public string id;
+    public int id;
  
     private string[] firmArr = new string[6] { "CyberPower", "IPPON", "El-Power", "Philips","Panasonic","samsung" };
     private string[] markArr = new string[4] { "Резервный(Off-Line)", "Линейно-интерактивный(Line-Interactive)", "с двойным преобразованием напряжения(On-Line)","Cломанный" };
@@ -63,9 +76,11 @@ public class PowerSuply
         int t2 = random.Next(0, markArr.Length);
         firm = firmArr[t];
         mark = markArr[t2];
-        id = Guid.NewGuid().ToString("N");
+        Random idRan = new Random();
+        id = idRan.Next(30);
 
     }
+    
 }
 
  
--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! I am making lab 66666654356476");
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Lab6;
+
+namespace Lab6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кол-во элементов");
+            int n = Convert.ToInt32(Console.ReadLine());
+            queues a = new queues(n);
+            a.GetIds();
+
+            Console.WriteLine("\nSORTEEEDDD\n");
+
+            Sorts.DirectChoose(a);
+            a.GetIds();
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
