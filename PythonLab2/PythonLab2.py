44722fca50e48dbe3079fa1fbf7516e811631610
AyazKhai <ayazhhh04@gmail.com>
2023-11-19 23:32:28 +0300

    python lab2 cumIt

--- PythonLab2/PythonLab2.py
+++ PythonLab2/PythonLab2.py
@@ -1,13 +1,19 @@
-class PowerSupply:
-    def __init__(self, firm="Undefined", mark="Undefined", capcity=0, cost=0):
+﻿class PowerSuply:
+    def __init__(self, firm="Undefind", mark="Undefind", capacity=0, cost=0):
         self.firm = firm
         self.mark = mark
-        self.capcity = capcity
+        self.capacity = capacity
         self.cost = cost
+Myown = PowerSuply()
+print("Введите название вашего источника бесперибойного питания")
+Myown.firm = input()
+print("Введите марку вашего источника бесперибойного питания")
+Myown.mark = input()
+print("введите ёмкость вашего источника бесперибойного питания")
+Myown.capacity = int(input())
 
-if __name__ == "__main__":
-    Ecovolt = PowerSupply("Ecovolt", "Termo312", 300, 13300)
-    Myown = PowerSupply()
+Ecovolt = PowerSuply("Ecovolt", "Termo312", 300, 13300)
 
-    print("Power supply source name from the store: ", Ecovolt.firm, Ecovolt.mark)
-    print("Home power supply source name: ", Myown.firm, Myown.mark)
+print("Название источника бесперебойного питания из магазина", Ecovolt.firm, Ecovolt.mark)
+print("Название домашнего источника бесперебойного питания",Myown.firm, Myown.mark, Myown.capacity)
+
